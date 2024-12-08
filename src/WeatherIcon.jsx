39638d15644b7d691b6ee783d4db2a6ad7131152
Joemer <joemers97@gmail.com>
2024-12-08 10:56:53 -0500

    Adding components, weatherData.

--- src/WeatherIcon.jsx
+++ src/WeatherIcon.jsx
@@ -1,24 +1,7 @@
-// import React from 'react';
+import React from 'react';
 
-// const WeatherIcon = ({ img, alt }) => {
-//   return <img src={/WeatherIcon/windy.png}/> alt={alt} />;
-// };
-windy.png;
-
-// const WeatherIcon = ({ img, alt }) => {
-// 	return <img src={img} alt={alt} />;
-// };
-// const WeatherIcon = ({ img, alt }) => {
-// 	return <img src={/Users/joemersanchez/Downloads/windy_7059296.png} alt={alt} />;
-// };
-// const WeatherIcon = ({ img, alt }) => {
-// 	return <img src={/Users/joemersanchez/Downloads/weather-app_7059514.png} alt={alt} />;
-// };
-// const WeatherIcon = ({ img, alt }) => {
-// 	return <img src={/Users/joemersanchez/Downloads/1530390_weather_clouds_cloudy_rain_sunny_icon.png} alt={alt} />;
-// };
-// const WeatherIcon = ({ img, alt }) => {
-// 	return <img src={/Users/joemersanchez/Downloads/2995006_snow_forecast_snowflake_weather_cold_icon.png} alt={alt} />;
-// };
+const WeatherIcon = ({ img, alt }) => {
+	return <img src={img} alt={alt} />;
+};
 
 export default WeatherIcon;
